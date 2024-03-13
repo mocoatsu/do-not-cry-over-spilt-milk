240d6161eb8448bd9becddd3a9f5939e6c3b77a4
mocoatsu <mocomocoaimai673@gmail.com>
2024-03-13 21:57:26 +0900

    feature: Add drink and pour actions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 const MAX_MILK: usize = 5;
 
 struct MilkBucket {
@@ -9,11 +11,20 @@ impl MilkBucket {
         MilkBucket { milk: 0 }
     }
 
-    pub fn add_milk(&mut self) -> bool {
+    pub fn pour_milk(&mut self) -> bool {
         if self.milk >= MAX_MILK {
-            false
+            return false;
         } else {
             self.milk += 1;
+            return true;
+        }
+    }
+
+    pub fn dring_milk(&mut self) -> bool {
+        if self.milk == 0 {
+            false
+        } else {
+            self.milk -= 1;
             true
         }
     }
@@ -24,6 +35,27 @@ fn main() {
 
     loop {
         println!("Press `p` to pour milk, `d` to drink, or `q` to quit");
+
+        let mut action = String::new();
+        io::stdin()
+            .read_line(&mut action)
+            .expect("Failed to read line");
+
+        match action.trim() {
+            "p" => {
+                if !bucket.pour_milk() {
+                    println!("Don't cry over split milk");
+                    break;
+                }
+            }
+            "d" => {
+                if !bucket.dring_milk() {
+                    println!("No milk left to drink");
+                }
+            }
+            "q" => break,
+            _ => println!("Invalid input. Please try again."),
+        }
     }
 }
 
@@ -32,10 +64,20 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_add_milk() {
+    fn test_pour_milk() {
         let mut bucket = MilkBucket::new();
-        bucket.milk = MAX_MILK;
+        bucket.milk = MAX_MILK - 1;
 
-        assert!(!bucket.add_milk())
+        assert!(bucket.pour_milk());
+        assert!(!bucket.pour_milk())
+    }
+
+    #[test]
+    fn test_drink_milk() {
+        let mut bucket = MilkBucket::new();
+        bucket.milk += 1;
+
+        assert!(bucket.dring_milk());
+        assert!(!bucket.dring_milk());
     }
 }
